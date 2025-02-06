e2f1ad94ff2bcce7fd4a16fa6976cb0ed2fdc3aa
Alena Stavrova <33433876+Alena-Stavrova@users.noreply.github.com>
2025-02-06 09:03:36 +0300

    Update pagination_v1.py

--- pagination_v1.py
+++ pagination_v1.py
@@ -1,5 +1,5 @@
-#A script that goes to the next page of a section of a catalog.
-#It's working unreliably, so I'd like to improve it in the future (got a few ideas)
+#A script that goes through a category (can be 1 or more pages)
+#and collects all the skus. Useful for checking the catalog.
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,28 +9,69 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-#Copy and paste the link to an item on ERM CZ
-website_erm = input("Ссылка на CZ ERM: ")
-
-# Create a new instance of the Chrome driver
+website_erm = input("Ссылка на категорию CZ ERM: ")
 driver = webdriver.Chrome()
 
-def next_page(my_link):
-    driver.get(my_link)
-    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+def process_category_page(url, page_number):
+    load_page(url, page_number) 
+    page_skus = parse_skus()
+    next_page_present = detect_next_page_button()
+    return(page_skus, next_page_present) 
 
-    #Check if there are any other pages
+def load_page(url, page_n): #should contain all the manipulations with the url
+    if page_n == 1:
+        target_url = url
+    else:
+        target_url = url + "/?PAGEN_1=" + str(page_n)
+    driver.get(target_url)
+    #time.sleep(3)
+
+def parse_skus():
+    skus = []
+    while True:
+        time.sleep(1)
+        elems = driver.find_elements(By.CSS_SELECTOR, ".product-card__article")
+        emptyElem = None
+        for el in elems:
+            if el.text == '':
+                emptyElem = el
+                #print(emptyElem.text)
+                break
+            new_sku = el.text[4:]
+            if new_sku not in skus:
+                skus.append(new_sku)
+        if emptyElem is None:
+            break
+        driver.execute_script("arguments[0].scrollIntoView(true)", emptyElem)
+        print("retrying")
+    return(skus)
+    #print("Найдено " + str(len(skus)) + " товаров")
+
+def detect_next_page_button():
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
     try:
         elems = driver.find_elements(By.CSS_SELECTOR, "div.pagination__list a.btn")
         current_page = driver.find_element(By.CSS_SELECTOR, "div.pagination__list a.btn.btn-secondary.active").text
-        #print(current_page)
-        elems[int(current_page)].click() #list starts with 0
-        time.sleep(5)
-        
+        #print(len(elems), current_page)
+        if int(current_page) < len(elems):
+            return(True)
+        else:
+            return(False)            
     except NoSuchElementException:
-        print("One page only") 
+        return(False)
 
-next_page(website_erm)
+def main(url):
+    all_skus = []
+    page = 1
+    while True:
+        page_skus, has_next = process_category_page(url, page)
+        #print('page', page, 'contains skus:', page_skus)
+        all_skus += page_skus #check if this syntax works
+        if not has_next:
+            break
+        page += 1
+    print(all_skus)
+    print("Всего найдено " + str(len(all_skus)) + " SKUs")
 
-# Close the browser
-driver.quit()
+main(website_erm)
